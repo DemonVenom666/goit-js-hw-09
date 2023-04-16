a9ed5bfff19590c1aa3caa511aa62b356b6c5b31
DemonVenom666 <116897771+DemonVenom666@users.noreply.github.com>
2023-04-16 12:57:18 +0200

    fixed

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,25 +1,61 @@
-function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+// function getRandomHexColor() {
+//     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
+
+// const refs = {
+//     startBtnChangeColor: document.querySelector('[data-start]'),
+//     stopBtnColor: document.querySelector('[data-stop]'),
+//     allBody: document.querySelector('body')
+// };
+
+// let onChangeColor = null;
+
+// refs.startBtnChangeColor.addEventListener('click', changeColor);
+// refs.stopBtnColor.addEventListener('click', stopChangeColor);
+// refs.stopBtnColor.setAttribute('disabled', true);
+
+// function changeColor() {
+//     onChangeColor = setInterval(() => {
+//         refs.allBody.style.backgroundColor = getRandomHexColor();
+//         refs.startBtnChangeColor.setAttribute('disabled', true);
+//         refs.stopBtnColor.removeAttribute('disabled', true);
+//     }, 1000);
+
+// };
+
+// function stopChangeColor() {
+//     clearInterval(onChangeColor);
+//     refs.stopBtnColor.setAttribute('disabled', true);
+//     refs.startBtnChangeColor.removeAttribute('disabled', true);
+// };
 
 const refs = {
-    startBtnChangeColor: document.querySelector('[data-start]'),
-    stopBtnColor: document.querySelector('[data-stop]'),
-    allBody: document.querySelector('body'),
+    start: document.querySelector('button[data-start]'),
+    stop: document.querySelector('button[data-stop]'),
 };
 
-refs.startBtnChangeColor.addEventListener('click', changeColor);
-refs.stopBtnColor.addEventListener('click', stopChangeColor);
-let onChangeColor = null;
+let timerId = null;
 
-function changeColor() {
-    onChangeColor = setInterval(() => {
-        refs.allBody.style.backgroundColor = getRandomHexColor();
+refs.start.addEventListener('click', changeBodyColor);
+refs.stop.addEventListener('click', stopChangeBodyColor);
+refs.stop.setAttribute('disabled', true);
+
+function changeBodyColor() {
+    timerId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+        refs.start.setAttribute('disabled', true);
+        refs.stop.removeAttribute('disabled', true);
     }, 1000);
-    refs.startBtnChangeColor.disabled = true;
-};
+}
 
-function stopChangeColor() {
-    clearInterval(onChangeColor);
-    refs.startBtnChangeColor.disabled = false;
-};
+function stopChangeBodyColor() {
+    clearInterval(timerId);
+    refs.stop.setAttribute('disabled', true);
+    refs.start.removeAttribute('disabled', true);
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215)
+        .toString(16)
+        .padStart(6, 0)}`;
+}
