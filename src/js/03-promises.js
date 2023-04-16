19bf7eb3962604b4397d6433368afc28f12593dc
DemonVenom666 <116897771+DemonVenom666@users.noreply.github.com>
2023-04-16 17:41:03 +0200

    fixed

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,7 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const refs = {
-  form: document.querySelector('.form'),
-  btn: document.querySelector('.submit')
+  form: document.querySelector('.form')
 };
 
 refs.form.addEventListener('submit', onSubmitClick);
@@ -18,30 +17,45 @@ function onSubmitClick(event) {
   step = Number(step.value);
   amount = Number(amount.value);
 
-  for (let i = 1; i <= amount; i += 1) {
+  for (let i = 1; i <= amount; i++) {
     createPromise(i, delay)
       .then(({ position, delay }) => {
         Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`, { useIcon: false });
       })
       .catch(({ position, delay }) => {
         Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`, { useIcon: false });
-      });
-    delay += step;
+      })
+      // delay += step;
+      // .finally(() => form.reset());
+      .finally(() => form.reset());
   }
-  refs.btn.disabled = true;
 
-}
+  // for (let i = 0; i < amount; i++) {
+  //   const position = i + 1;
+  //   const delay = firstDelay + i * delaySteep;
+  //   createPromise(position, delay)
+  //     .then(({ position, delay }) => {
+  //       console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  //       Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  //     })
+  //     .catch(({ position, delay }) => {
+  //       console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  //       Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+  //     })
+  //     .finally(() => form.reset());
+  // }
 
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
+  function createPromise(position, delay) {
+    const shouldResolve = Math.random() > 0.3;
 
-  return new Promise((fulfill, reject) => {
-    setTimeout(() => {
-      if (shouldResolve) {
-        fulfill({ position, delay });
-      } else {
-        reject({ position, delay });
-      }
-    }, delay);
-  });
+    return new Promise((fulfill, reject) => {
+      setTimeout(() => {
+        if (shouldResolve) {
+          fulfill({ position, delay });
+        } else {
+          reject({ position, delay });
+        }
+      }, delay);
+    });
+  }
 }
